a7f6668f7a9196066012cf3c0d8fd86768625c63
Liam Allan <WorksOfBarry@users.noreply.github.com>
2017-01-23 09:28:39 +0000

    Update db2.lua

--- tests/db2.lua
+++ tests/db2.lua
@@ -11,7 +11,7 @@ do
   local stmt = db2.allocStatement(hdl)
   print(stmt)
   
-  local execres = db2.executeStatement(stmt, "delete from pjstest.productsp where PRID = 107") 
+  local execres = db2.executeStatement(stmt, "delete from file where id = 107") 
   print(execres)
   
   db2.printError(env, hdl, stmt)
